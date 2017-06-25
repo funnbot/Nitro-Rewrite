4f7e02904266a65add64b65e7e0b35783dbe3fc2
funnbot <unconfigured@null>
2017-06-25 10:56:56 -0700

    a

--- modules/tag/bot.js
+++ modules/tag/bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 
-const message = reqire('./message.js')
+const message = require('./message.js')
 const { TOKEN } = require('../../config.js')
 
 const bot = new Discord.Client({
