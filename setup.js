8268db4ca7105a1fae86a7f6c476f61d07bf1040
Alex Waese-Perlman <squishybanana04@gmail.com>
2017-08-05 18:21:01 -0400

    Setup script

---
+++ setup.js
@@ -0,0 +1,38 @@
+const tables = [
+  "alias",
+  "economy",
+  "mod",
+  "perms",
+  "prefix",
+  "roles",
+  "tag"
+]
+
+const r = require("rethinkdbdash")()
+
+console.log("running")
+async function setup () {
+  async function createDbs (existingDbs) {
+    if (existingDbs.includes("Nitro")) {
+      createTables(await r.db("Nitro").tableList().run())
+    }
+    else {
+      console.log("creating a database called Nitro")
+      r.dbCreate("Nitro").run(() => {
+        createDbs(["Nitro"])
+      })
+    }
+  }
+
+  function createTables (existingTables) {
+    for (const table of tables) {
+      if (!existingTables.includes(table)) {
+        console.log("created a table called " + table)
+        r.db("Nitro").tableCreate(table).run()
+      }
+    }
+  }
+
+  createDbs(await r.dbList().run())
+}
+setup()
