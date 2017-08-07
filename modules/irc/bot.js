361e89155bd29ad9e06be27389d6081522930044
Alex <squishybanana04@users.noreply.github.com>
2017-08-07 00:09:16 -0400

    Update bot.js

--- modules/irc/bot.js
+++ modules/irc/bot.js
@@ -1,6 +1,6 @@
 const Client = require("../../struct/Client.js")
 const client = new Client("irc")
 client.database(["irc"])
-module.exports = client.bot
+module.exports = client.bot;
 require("./message.js")
 client.login()
