ed05f47b8b228ddc711be0820c948795641b8e0b
funnbot <unconfigured@null>
2017-08-07 12:24:04 -0700

    SnowFlakeUtil

--- modules/dev/SnowFlakeUtil.js
+++ modules/dev/SnowFlakeUtil.js
@@ -4,53 +4,17 @@ const Long = require("long")
 const EPOCH = 1420070400000
 let INCREMENT = 0
 
-/**
- * A container for useful snowflake-related methods.
- */
 class SnowflakeUtil {
   constructor() {
     throw new Error(`The ${this.constructor.name} class may not be instantiated.`)
   }
 
-  /**
-   * A Twitter snowflake, except the epoch is 2015-01-01T00:00:00.000Z
-   * ```
-   * If we have a snowflake '266241948824764416' we can represent it as binary:
-   *
-   * 64                                          22     17     12          0
-   *  000000111011000111100001101001000101000000  00001  00000  000000000000
-   *       number of ms since Discord epoch       worker  pid    increment
-   * ```
-   * @typedef {string} Snowflake
-   */
-
-  /**
-   * Generates a Discord snowflake.
-   * <info>This hardcodes the worker ID as 1 and the process ID as 0.</info>
-   * @returns {Snowflake} The generated snowflake
-   */
   static generate() {
     if (INCREMENT >= 4095) INCREMENT = 0
     const BINARY = `${pad((Date.now() - EPOCH).toString(2), 42)}0000100000${pad((INCREMENT++).toString(2), 12)}`
     return Long.fromString(BINARY, 2).toString()
   }
 
-  /**
-   * A deconstructed snowflake.
-   * @typedef {Object} DeconstructedSnowflake
-   * @property {number} timestamp Timestamp the snowflake was created
-   * @property {Date} date Date the snowflake was created
-   * @property {number} workerID Worker ID in the snowflake
-   * @property {number} processID Process ID in the snowflake
-   * @property {number} increment Increment in the snowflake
-   * @property {string} binary Binary representation of the snowflake
-   */
-
-  /**
-   * Deconstructs a Discord snowflake.
-   * @param {Snowflake} snowflake Snowflake to deconstruct
-   * @returns {DeconstructedSnowflake} Deconstructed snowflake
-   */
   static deconstruct(snowflake) {
     const BINARY = pad(Long.fromString(snowflake).toString(2), 64)
     const res = {
