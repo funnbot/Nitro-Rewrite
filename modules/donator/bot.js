63836699272fa5b2314abc6c84270f43d8f8619d
Gymnophoria <buddercreeperman@gmail.com>
2017-08-06 21:24:54 -0700

    actually added the message mabob and database mabob, funnbot is shit at explaining things imo

--- modules/donator/bot.js
+++ modules/donator/bot.js
@@ -1,5 +1,9 @@
+const Nitro = require('../../Nitro.js');
 const Client = require("../../struct/Client.js");
 const client = new Client("tag");
+
+client.database();
+Nitro.Message(client.bot);
 
 module.exports = client.bot;
 
