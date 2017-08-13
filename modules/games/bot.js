5b7ab0942c160b968f75b3f2945c1e847ff9c965
funnbot <unconfigured@null>
2017-08-12 22:20:32 -0700

    Fixed game/bot.js

--- modules/games/bot.js
+++ modules/games/bot.js
@@ -1,6 +1,6 @@
 const Client = require("../../struct/Client.js")
-const client = new Client("economy")
-client.database(["economy"])
+const client = new Client("game")
+client.database(["game", "economy"])
 module.exports = client.bot
 const MoneyManager = require("./MoneyManager.js")
 client.bot.moneyman = new MoneyManager()
